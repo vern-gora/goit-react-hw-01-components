35727e8e0c8220cfb75a2601a4587bdfb9ef9365
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-07-20 00:47:36 +0300

    add profile and statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
+import user from './DataJSON/user.json';
+import data from './DataJSON/data.json';
+
+import Profile from './Profile';
+import Statistics from './Statistics';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </>
   );
 };
+
+export default App;
