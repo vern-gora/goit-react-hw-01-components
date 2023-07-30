8b062d461b1d533a5f6ae90cfbb9f3caab5f1459
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-07-30 19:57:36 +0300

    fix3

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 
-const FriendListItem = ({ id, avatar, name, isOnline }) => {
+const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li className={css.item} key={id}>
+    <li className={css.item}>
       <span className={isOnline ? css['status-on'] : css['status-off']}></span>
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -10,7 +10,7 @@ function getRandomHexColor() {
 const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title !== '' && <h2 className={css.title}>{title}</h2>}
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(stat => (
           <li
